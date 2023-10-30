b8551c322e6d5f69c169aeb5793230a5f268cad6
arlinggamw <149373969+arlinggamw@users.noreply.github.com>
2023-10-30 17:09:57 +0700

    Update update.py

--- update.py
+++ update.py
@@ -1,54 +1,21 @@
-pip install gensim
-
-
 import streamlit as st
 import pandas as pd
-import gensim
-from gensim import corpora
-from gensim.models import LdaModel
-import pyLDAvis.gensim_models
 
 # Judul Aplikasi
-st.title("Analisis Topic Modeling dengan Streamlit")
+st.title("Aplikasi Streamlit Sederhana")
 
-# Sidebar
-st.sidebar.subheader("Pengaturan")
-num_topics = st.sidebar.slider("Jumlah Topik", min_value=2, max_value=20, value=5)
-
-# Upload Dataset
-st.sidebar.subheader("Upload Dataset")
-uploaded_file = st.sidebar.file_uploader("Pilih file CSV", type=["csv"])
+# Pilihan Dataset
+uploaded_file = st.file_uploader("Unggah file CSV", type=["csv"])
 
 if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
+    data = 'punk'
 
-    # Data Preprocessing (tokenisasi, stop words removal, dll.)
+    # Tampilkan Dataset
+    st.subheader("Data:")
+    st.write(data)
 
-    # Membangun corpus dan dictionary
-    text_data = data['text'].tolist()
-    dictionary = corpora.Dictionary(text_data)
-    corpus = [dictionary.doc2bow(text) for text in text_data]
-
-    # Membangun model LDA
-    lda_model = LdaModel(corpus, num_topics=num_topics, id2word=dictionary, passes=15)
-
-    # Visualisasi dengan pyLDAvis
-    vis_data = pyLDAvis.gensim_models.prepare(lda_model, corpus, dictionary)
-
-    # Tampilkan Visualisasi Topik
-    st.write("Visualisasi Topik:")
-    st.pydeck_chart(vis_data)
-
-    # Pilihan Topik
-    st.sidebar.subheader("Pilihan Topik")
-    selected_topic = st.sidebar.selectbox("Pilih Topik", range(num_topics))
-
-    # Tampilkan Dokumen dalam Topik yang Dipilih
-    st.write(f"Topik yang Dipilih: Topik {selected_topic + 1}")
-    st.subheader("Dokumen dalam Topik yang Dipilih:")
-    documents_in_topic = [i for i, topics in enumerate(lda_model.get_document_topics(corpus)) if selected_topic in [topic[0] for topic in topics]]
-    for doc_index in documents_in_topic:
-        st.write(f"Dokumen #{doc_index + 1}:")
-        st.write(data['text'][doc_index])
+    # Statistik Dataset
+    st.subheader("Statistik Data:")
+    st.write(data.describe())
 else:
-    st.write("Silakan unggah dataset untuk memulai analisis topic modeling.")
+    st.write("Silakan unggah dataset untuk memulai analisis.")
