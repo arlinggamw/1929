e14c87487efaff09806c36ea5d560039a4163f6b
arlinggamw <149373969+arlinggamw@users.noreply.github.com>
2023-10-30 16:53:49 +0700

    Update update.py

--- update.py
+++ update.py
@@ -1,19 +1,37 @@
 import streamlit as st
-import pickle
-import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 
-# Muat model ML Anda (di sini menggunakan contoh Regresi Linear)
-#model = pickle.load(open('model.pkl', 'rb'))
+# Fungsi untuk pemodelan topik
+def run_lda(df, num_topics):
+    vectorizer = CountVectorizer(max_df=0.95, min_df=2, stop_words='english')
+    data_vectorized = vectorizer.fit_transform(df['text'])
 
-st.title('Aplikasi Deployment Model ML dengan Streamlit')
+    lda = LatentDirichletAllocation(n_components=num_topics, random_state=42)
+    lda.fit(data_vectorized)
 
-# Masukkan fitur yang diperlukan
-feature1 = st.slider('Feature 1', 0.0, 100.0, 0.0)
-feature2 = st.slider('Feature 2', 0.0, 100.0, 0.0)
+    return lda
 
-# Prediksi dengan model
-input_features = np.array([feature1, feature2]).reshape(1, -1)
-#prediction = model.predict(input_features)
+# Tampilan Streamlit
+st.title('Aplikasi Pemodelan Topik')
 
-#st.write('Hasil Prediksi:', prediction[0])
-st.write('mantap')
+# Unggah dokumen
+st.write('### Unggah Dokumen')
+uploaded_file = st.file_uploader('Unggah file CSV yang berisi teks dokumen', type=['csv'])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head())
+
+    num_topics = st.slider('Pilih Jumlah Topik', 2, 10, 4)
+
+    if st.button('Mulai Pemodelan'):
+        lda_model = run_lda(df, num_topics)
+        st.success('Pemodelan selesai!')
+
+        # Tampilkan kata-kata kunci untuk setiap topik
+        st.write('### Kata-kata Kunci Topik:')
+        for i, topic in enumerate(lda_model.components_):
+            top_words = [vectorizer.get_feature_names_out()[i] for i in topic.argsort()[-5:][::-1]]
+            st.write(f'Topik {i+1}: {", ".join(top_words)}')
