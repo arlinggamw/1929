c8b59f55df77e121d1e1923839024f589b71915b
arlinggamw <149373969+arlinggamw@users.noreply.github.com>
2023-10-30 17:05:45 +0700

    Update update.py

--- update.py
+++ update.py
@@ -5,40 +5,47 @@ from gensim import corpora
 from gensim.models import LdaModel
 import pyLDAvis.gensim_models
 
-# Load data
-data = 'punk'  # Gantilah "data.csv" dengan nama file data Anda
+# Judul Aplikasi
+st.title("Analisis Topic Modeling dengan Streamlit")
 
-# Preprocessing data (misalnya: tokenisasi, stop words removal, stemming, dll.)
-
-# Membangun corpus dan dictionary
-text_data = data['text'].tolist()
-dictionary = corpora.Dictionary(text_data)
-corpus = [dictionary.doc2bow(text) for text in text_data]
-
-# Membangun model LDA
+# Sidebar
+st.sidebar.subheader("Pengaturan")
 num_topics = st.sidebar.slider("Jumlah Topik", min_value=2, max_value=20, value=5)
-lda_model = LdaModel(corpus, num_topics=num_topics, id2word=dictionary, passes=15)
 
-# Tampilkan topik
-st.sidebar.subheader("Topik Utama")
-for topic in range(num_topics):
-    st.sidebar.write(f"Topik {topic + 1}: {lda_model.print_topic(topic)}")
+# Upload Dataset
+st.sidebar.subheader("Upload Dataset")
+uploaded_file = st.sidebar.file_uploader("Pilih file CSV", type=["csv"])
 
-# Visualisasi dengan pyLDAvis
-vis_data = pyLDAvis.gensim_models.prepare(lda_model, corpus, dictionary)
-st.write("Visualisasi Topik:")
-st.pydeck_chart(vis_data)
+if uploaded_file is not None:
+    data = 'punk'
 
-# Tampilkan dokumen dan topik yang relevan
-st.sidebar.subheader("Analisis Dokumen")
-document_index = st.sidebar.number_input("Nomor Dokumen (0 - 999)", min_value=0, max_value=len(data) - 1)
-document = text_data[document_index]
-document_bow = dictionary.doc2bow(document)
-document_topics = lda_model.get_document_topics(document_bow)
+    # Data Preprocessing (tokenisasi, stop words removal, dll.)
 
-st.write(f"Dokumen #{document_index}:")
-st.write(data['text'][document_index])
+    # Membangun corpus dan dictionary
+    text_data = data['text'].tolist()
+    dictionary = corpora.Dictionary(text_data)
+    corpus = [dictionary.doc2bow(text) for text in text_data]
 
-st.write("Topik yang relevan:")
-for topic, score in document_topics:
-    st.write(f"Topik {topic + 1}: {score:.2f}")
+    # Membangun model LDA
+    lda_model = LdaModel(corpus, num_topics=num_topics, id2word=dictionary, passes=15)
+
+    # Visualisasi dengan pyLDAvis
+    vis_data = pyLDAvis.gensim_models.prepare(lda_model, corpus, dictionary)
+
+    # Tampilkan Visualisasi Topik
+    st.write("Visualisasi Topik:")
+    st.pydeck_chart(vis_data)
+
+    # Pilihan Topik
+    st.sidebar.subheader("Pilihan Topik")
+    selected_topic = st.sidebar.selectbox("Pilih Topik", range(num_topics))
+
+    # Tampilkan Dokumen dalam Topik yang Dipilih
+    st.write(f"Topik yang Dipilih: Topik {selected_topic + 1}")
+    st.subheader("Dokumen dalam Topik yang Dipilih:")
+    documents_in_topic = [i for i, topics in enumerate(lda_model.get_document_topics(corpus)) if selected_topic in [topic[0] for topic in topics]]
+    for doc_index in documents_in_topic:
+        st.write(f"Dokumen #{doc_index + 1}:")
+        st.write(data['text'][doc_index])
+else:
+    st.write("Silakan unggah dataset untuk memulai analisis topic modeling.")
