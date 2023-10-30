4b76af31b80a06a63898288d5aac9415cf9e19cb
arlinggamw <149373969+arlinggamw@users.noreply.github.com>
2023-10-30 17:00:51 +0700

    Update update.py

--- update.py
+++ update.py
@@ -5,40 +5,61 @@ from gensim import corpora
 from gensim.models import LdaModel
 import pyLDAvis.gensim_models
 
-# Load data
-data = pd.read_csv("data.csv")  # Gantilah "data.csv" dengan nama file data Anda
+# Judul Aplikasi
+st.title("Aplikasi Analisis Topic Modeling")
 
-# Preprocessing data (misalnya: tokenisasi, stop words removal, stemming, dll.)
-
-# Membangun corpus dan dictionary
-text_data = data['text'].tolist()
-dictionary = corpora.Dictionary(text_data)
-corpus = [dictionary.doc2bow(text) for text in text_data]
-
-# Membangun model LDA
+# Sidebar
+st.sidebar.subheader("Pengaturan")
 num_topics = st.sidebar.slider("Jumlah Topik", min_value=2, max_value=20, value=5)
-lda_model = LdaModel(corpus, num_topics=num_topics, id2word=dictionary, passes=15)
 
-# Tampilkan topik
-st.sidebar.subheader("Topik Utama")
-for topic in range(num_topics):
-    st.sidebar.write(f"Topik {topic + 1}: {lda_model.print_topic(topic)}")
+# Pilihan Dataset (contoh: pilihan statis)
+dataset_option = st.sidebar.radio("Pilih Dataset", ["Dataset 1", "Dataset 2", "Dataset 3"])
 
-# Visualisasi dengan pyLDAvis
-vis_data = pyLDAvis.gensim_models.prepare(lda_model, corpus, dictionary)
-st.write("Visualisasi Topik:")
-st.pydeck_chart(vis_data)
+# Visualisasi Topik (contoh: grafik statis)
+st.sidebar.subheader("Visualisasi Topik")
+st.sidebar.image("grafik_topik.png", use_container_width=True)
 
-# Tampilkan dokumen dan topik yang relevan
-st.sidebar.subheader("Analisis Dokumen")
-document_index = st.sidebar.number_input("Nomor Dokumen (0 - 999)", min_value=0, max_value=len(data) - 1)
-document = text_data[document_index]
-document_bow = dictionary.doc2bow(document)
-document_topics = lda_model.get_document_topics(document_bow)
+# Informasi Tambahan (contoh: teks statis)
+st.sidebar.subheader("Informasi Topik")
+st.sidebar.write("Topik Utama: Topik 1")
+st.sidebar.write("Deskripsi: Ini adalah topik utama yang membahas topik-topik penting.")
 
-st.write(f"Dokumen #{document_index}:")
-st.write(data['text'][document_index])
+# Data Mockup
+data = pd.DataFrame({
+    'Dokumen': range(1, 11),
+    'Teks Dokumen': [
+        "Ini adalah contoh dokumen 1.",
+        "Ini adalah contoh dokumen 2.",
+        "Ini adalah contoh dokumen 3.",
+        "Ini adalah contoh dokumen 4.",
+        "Ini adalah contoh dokumen 5.",
+        "Ini adalah contoh dokumen 6.",
+        "Ini adalah contoh dokumen 7.",
+        "Ini adalah contoh dokumen 8.",
+        "Ini adalah contoh dokumen 9.",
+        "Ini adalah contoh dokumen 10."
+    ]
+})
+
+# Tampilan Utama
+st.subheader("Visualisasi Topik")
+st.image("visualisasi_topik.png", use_container_width=True)
+
+st.subheader("Daftar Dokumen")
+st.dataframe(data, width=600, height=300)
+
+selected_document = st.selectbox("Pilih Dokumen", data['Dokumen'])
+document_text = data[data['Dokumen'] == selected_document]['Teks Dokumen'].values[0]
+
+st.subheader("Analisis Dokumen")
+st.write(f"Dokumen #{selected_document}:")
+st.write(document_text)
 
 st.write("Topik yang relevan:")
-for topic, score in document_topics:
-    st.write(f"Topik {topic + 1}: {score:.2f}")
+st.write("Topik 1: 0.75")
+st.write("Topik 2: 0.20")
+st.write("Topik 3: 0.05")
+
+# Informasi Tambahan (contoh: teks statis)
+st.sidebar.subheader("Tentang Aplikasi")
+st.sidebar.write("Aplikasi ini digunakan untuk menganalisis topik dalam dataset teks menggunakan model Topic Modeling.")
